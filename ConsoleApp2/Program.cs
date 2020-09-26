1ebfa9ed9e6113a9fcae7dbe9a38c1c95bd3e825
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2020-09-26 21:36:36 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int nummer = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Vælg et nummer mellem 1 og 100");
+                int i = Convert.ToInt32(Console.ReadLine());
+                if (i > nummer)
+                {
+                    Console.WriteLine("For højt! gæt lavere");
+                }
+                else if (i < nummer)
+                {
+                    Console.WriteLine("For lavt!! gæt højere");
+
+                }
+                else if (i == nummer)
+                {
+                    Console.WriteLine("DU GÆTTEDE RIGTIG");
+                    win = true;
+                }
+
+
+                } while (win == false);
+            Console.WriteLine("Tak fordi du spillede med!");
+            Console.ReadKey();
+                
+              
+            
+        }
+    }
+}
